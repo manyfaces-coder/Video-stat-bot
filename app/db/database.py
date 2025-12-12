5123e4c7bbebc67bcea797a7ca65f0ea4b215269
manyfaces-coder <semenovoleg99@yandex.ru>
2025-12-13 01:22:49 +0300

    Добавлено создание таблиц в БД по описанным моделям

--- app/db/database.py
+++ app/db/database.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
+from app.db.models import Base
 
 # грузим .env (временный простой вариант)
 load_dotenv(find_dotenv())
@@ -12,7 +13,10 @@ DB_USER = os.getenv("POSTGRES_USER")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
 
-# DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 DATABASE_URL = f"postgresql+psycopg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
