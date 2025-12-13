fa7273b2d01ebf05882e353c14d23cad56ad3d59
manyfaces-coder <semenovoleg99@yandex.ru>
2025-12-13 16:11:07 +0300

    Реализована минимальная работа бота: 1) Ловит команду из чата с пользователем 2) Отправляет запрос в FastApi 3) Получает ответ и отправляет его пользователю

--- app/bot/main.py
+++ app/bot/main.py
@@ -1,14 +1,38 @@
 import os
 import asyncio
-from aiogram import Bot, Dispatcher
+import aiohttp
+from aiogram import Bot, Dispatcher, Router, types, F
+from aiogram.filters import CommandStart, Command
 from dotenv import load_dotenv, find_dotenv
+
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def handle_start(message: types.Message):
+    await message.answer("Привет!")
+
+
+@router.message(Command('api'))
+async def handler_info(message: types.Message):
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get('http://127.0.0.1:8000/info') as resp:
+                data = await resp.json()
+                await message.answer(f"Ответ от API: {data}")
+
+    except Exception as e:
+        await message.answer(f"Ошибка при запросе к API: {e}")
+
 
 async def main():
     load_dotenv(find_dotenv())
-    bot = Bot(token=os.getenv('TOKEN'))
+    bot = Bot(token=os.getenv('BOT_TOKEN'))
     dp = Dispatcher()
+    dp.include_router(router)
 
     await dp.start_polling(bot)
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     asyncio.run(main())
